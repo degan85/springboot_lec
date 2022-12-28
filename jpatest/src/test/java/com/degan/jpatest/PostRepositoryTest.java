d7d709d410ed2a1ed11240e4e2227d1bcde6869a
CHO DAE GEUN <chodaegeun@CHOui-iMac.local>
2022-12-28 23:35:33 +0900

    Added save persistence state test

--- jpatest/src/test/java/com/degan/jpatest/PostRepositoryTest.java
+++ jpatest/src/test/java/com/degan/jpatest/PostRepositoryTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -17,6 +20,9 @@ import com.degan.jpatest.repository.PostRepository;
 // @SpringBootTest
 @DataJpaTest
 public class PostRepositoryTest {
+
+    @PersistenceContext
+    EntityManager entityManager;
     
     @Autowired
     PostRepository postRepository;
@@ -49,6 +55,45 @@ public class PostRepositoryTest {
         assertThat(page.getTotalElements()).isEqualTo(1);
         assertThat(page.getNumber()).isEqualTo(0);
 
+    }
+
+    @Test
+    @Rollback(false)
+    public void persistenceTest() {
+        Post post = new Post();
+        post.setTitle("jpa");
+        Post savedPost = postRepository.save(post);
+
+        Post postUpdate = new Post();
+        postUpdate.setId(post.getId());
+        postUpdate.setTitle("jpa2");
+        Post updated = postRepository.save(postUpdate);
+
+        postUpdate.setTitle("jpa3");
+        updated.setTitle("hh");
+        System.out.println("@@@"+postUpdate.toString());
+    }
+
+    @Test
+    public void saveTest() {
+
+        Post post = new Post();
+        post.setTitle("jpa");
+        Post savedPost = postRepository.save(post);
+
+        assertThat(entityManager.contains(savedPost)).isTrue();
+        assertThat(entityManager.contains(post)).isTrue();
+
+        Post postUpdate = new Post();
+        postUpdate.setId(post.getId());
+        postUpdate.setTitle("jpa2");
+        Post updatedPost = postRepository.save(postUpdate);
+
+        assertThat(entityManager.contains(updatedPost)).isTrue();
+        assertThat(entityManager.contains(postUpdate)).isFalse();
+
+        List<Post> all = postRepository.findAll();
+        assertThat(all.size()).isEqualTo(1);
     }
 
 }
